1ee600113fb76ec8cf282df992b2a9d0d76d4200
Kevin Gurton <kevin.gurton@gmail.com>
2020-05-12 11:07:53 +0100

    Jumped through tutorial hoop

--- vehicle.go
+++ vehicle.go
@@ -48,8 +48,10 @@ const (
 )
 
 func init() {
+	var vehicleResult map[string]feedbackResult
+	var inventory []vehicle
 
-	inventory := []vehicle{
+	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
 		car{"Sonata", "Hyundai", "Sedan"},
@@ -62,7 +64,7 @@ func init() {
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
 
-	vehicleResult := make(map[string]feedbackResult)
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
